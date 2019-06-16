7a5ad679ec8bab4d76ddcbe6c6b84f732cb00ff8
Rafael Castilla <rcastillal@gmail.com>
2019-06-16 12:32:13 +0200

    Input, Output y viewchild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,15 @@ import { APP_ROUTES } from './app.routes';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+
+
+
 
 
 
@@ -23,12 +29,15 @@ import { RegisterComponent } from './login/register.component';
     AppComponent,
     LoginComponent,
     RegisterComponent
+
   ],
   imports: [
     BrowserModule,
     APP_ROUTES,
     PagesModule,
-    SharedModule
+    SharedModule,
+    FormsModule
+
   ],
   providers: [
 
