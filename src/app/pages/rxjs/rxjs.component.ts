8368405f15e5b9354d28b7215183ae4fafb419b1
Rafael Castilla <rcastillal@gmail.com>
2019-10-03 19:11:25 +0200

    Primer commit Inventario Base Ordenada Modulos y rutas

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+export class RxjsComponent implements OnInit {
+
+  constructor() {
+
+    let obs = new Observable( observer => {
+
+      let contador = 0;
+
+      let intervalo = setInterval ( () => {
+
+        contador += 1;
+
+        observer.next( contador );
+        if ( contador === 3 ) {
+
+          clearInterval( intervalo );
+
+          observer.complete();
+        }
+
+        if ( contador === 2 ) {
+          clearInterval(intervalo);
+          observer.error('Auxilio!');
+        }
+
+      }, 1000 );
+
+    });
+
+    obs.pipe(
+      retry(2)
+        )
+
+    .subscribe(
+      numero => console.log( 'Subs ', numero ),
+      error => console.error('Error en el Obs', error),
+      () => console.log ('El observador termino!')
+    );
+  }
+
+  ngOnInit() {
+  }
+
+}
